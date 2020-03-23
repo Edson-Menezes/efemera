6c87521e632460c5309620bc5021dac0bad02857
Edson Menezes <menezes.edjr@gmail.com>
2020-03-23 01:31:03 -0300

    projeto efemera

---
+++ controllers/homeController.js
@@ -0,0 +1,22 @@
+const homeController = {
+    index: (req, res) => {
+      let servicos = [
+        {nome: 'Dev full stack', imagem: '/imagens/undraw_dev_focus.svg'},
+        {nome: 'Consultoria UX', imagem: '/imagens/undraw_mobile_apps.svg'},
+        {nome: 'Marketing Digital', imagem: '/imagens/undraw_social_dashboard.svg'},
+        {nome: 'Suporte tecnico', imagem: '/imagens/undraw_dev_focus.svg'},
+        {nome: 'Data Science', imagem: '/imagens/undraw_mobile_apps.svg'},
+      ];
+  
+      let banners = [
+        '/imagens/banner2.jpg',
+        '/imagens/banner3.jpg',
+        '/imagens/banner4.jpg',
+        '/imagens/banner.jpg',
+      ];
+  
+      res.render('index', { title: 'Home', listaServicos: servicos, listaBanners: banners });
+    }
+  };
+  
+  module.exports = homeController;
