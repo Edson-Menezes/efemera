105e97dec4da34821d102949dd23bc829b9418f1
Edson Menezes <menezes.edjr@gmail.com>
2020-03-25 00:02:49 -0300

    Metodo Get e Post

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,22 +1,37 @@
 const homeController = {
-    index: (req, res) => {
-      let servicos = [
-        {nome: 'Dev full stack', imagem: '/imagens/undraw_dev_focus.svg'},
-        {nome: 'Consultoria UX', imagem: '/imagens/undraw_mobile_apps.svg'},
-        {nome: 'Marketing Digital', imagem: '/imagens/undraw_social_dashboard.svg'},
-        {nome: 'Suporte tecnico', imagem: '/imagens/undraw_dev_focus.svg'},
-        {nome: 'Data Science', imagem: '/imagens/undraw_mobile_apps.svg'},
-      ];
-  
-      let banners = [
-        '/imagens/banner2.jpg',
-        '/imagens/banner3.jpg',
-        '/imagens/banner4.jpg',
-        '/imagens/banner.jpg',
-      ];
-  
-      res.render('index', { title: 'Home', listaServicos: servicos, listaBanners: banners });
-    }
-  };
-  
-  module.exports = homeController;
+  index: (req, res) => {
+    let servicos = [
+      {nome: 'Dev full stack', imagem: '/imagens/undraw_dev_focus.svg'},
+      {nome: 'Consultoria UX', imagem: '/imagens/undraw_mobile_apps.svg'},
+      {nome: 'Marketing Digital', imagem: '/imagens/undraw_social_dashboard.svg'},
+      {nome: 'Suporte tecnico', imagem: '/imagens/undraw_dev_focus.svg'},
+      {nome: 'Data Science', imagem: '/imagens/undraw_mobile_apps.svg'},
+    ];
+
+    let banners = [
+      '/imagens/banner2.jpg',
+      '/imagens/banner3.jpg',
+      '/imagens/banner4.jpg',
+      '/imagens/banner.jpg',
+    ];
+
+    res.render('index', { title: 'Home', listaServicos: servicos, listaBanners: banners });
+  },
+  contato: (req,res)=>{
+    let {nome, email, mensagem} =  req.body;
+
+    res.render('contato', {nome,email,mensagem});
+
+  },
+  newsletter: (req, res) => {
+    let {email} = req.query;
+    
+    // POST - req.body
+    // GET - req.query
+    // GET /:email - req.params
+
+    res.render('newsletter', {email, title: 'Newsletter'});
+  }
+};
+
+module.exports = homeController;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const homeController = require('../controllers/homeController')
+const homeController = require('../controllers/homeController');
+
 /* GET home page. */
 router.get('/', homeController.index);
+router.post('/contato', homeController.contato);
+router.get('/newsletter', homeController.newsletter);
 
 module.exports = router;
